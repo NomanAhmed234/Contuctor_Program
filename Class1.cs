cb61070956f6ffcdb4e901821f45939498c3c6d7
Noman Ahmed <nomanpb234@gmail.com>
2023-07-26 23:58:34 +0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contuctor
+{
+    internal class Class1
+    {
+       public int id;
+       public string name;
+        public char sec ;
+      
+        public Class1()
+        {
+
+        }
+        public Class1(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+       public Class1(int id, string name,char sec)
+        {
+            this.id = id;
+            this.name = name;
+            this.sec = sec;
+        }
+
+        public void detail()
+        {
+            Console.WriteLine("the id is {0}", id);
+            Console.WriteLine("the name is {0}", name);
+            Console.WriteLine("the section is {0}", sec);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Contuctor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            obj.detail();
+            Class1 obj1  = new Class1(070,"Noman");
+            obj1.detail();
+            Class1 obj2 = new Class1(555,"Ali",'B');
+            obj1.detail();
+
+        }
+    }
+}
